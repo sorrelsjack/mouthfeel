70bd2508dd4408e55f2e9ebfa2453c809f34bf17
Jack Sorrels <sorrelsjack@gmail.com>
2018-12-20 22:22:56 -0500

    Made a splash screen with a label and that's all. Splash screen doesn't work yet.

--- app/src/main/java/com/jacksorrels/mouthfeel/SplashScreen.java
+++ app/src/main/java/com/jacksorrels/mouthfeel/SplashScreen.java
@@ -1,16 +1,13 @@
 package com.jacksorrels.mouthfeel;
 
-import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    private static final int SPLASH_TIME_OUT = 4000;
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
