32cc95a37d994a0fec866d51d0142483ced5fb62
Jack Sorrels <sorrelsjack@gmail.com>
2018-12-22 00:27:04 -0500

    Splash screen now works as intended! Still needs to be styled and animated, but base functionality works.

--- app/src/main/java/com/jacksorrels/mouthfeel/SplashScreen.java
+++ app/src/main/java/com/jacksorrels/mouthfeel/SplashScreen.java
@@ -1,13 +1,26 @@
 package com.jacksorrels.mouthfeel;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
+    private static final int SPLASH_TIME_OUT = 4000;
 
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
